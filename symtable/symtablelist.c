32f5997f7debcd8adf14b651443bdebadef465c6
Nicholas Padmanabhan <nicholaspad@princeton.edu>
2020-10-09 22:36:34 -0700

    implemented new(), created Makefile

--- symtable/symtablelist.c
+++ symtable/symtablelist.c
@@ -5,16 +5,36 @@
 
 #include "symtable.h"
 
+static size_t length;
+
 struct Binding {
 	/* Pointer to binding key (string) */
 	const char *key;
 	/* Pointer to binding value */
 	void *value;
-	/* Pointer to next binding */
+	/* Pointer to next binding in list */
 	struct Binding *next;
 };
 
 struct SymTable {
-	/* Pointer to first binding */
+	/* Pointer to first binding in list */
 	struct Binding *first;
+	/* Number of bindings in list */
+	size_t length;
 };
+
+SymTable_T SymTable_new(void) {
+	SymTable_T *oSymTable = malloc(sizeof(SymTable_T));
+	if (oSymTable == NULL)
+		return NULL;
+
+	oSymTable->first = malloc(sizeof(struct Binding));
+	if (oSymTable->first == NULL) {
+		free(oSymTable);
+		return NULL;
+	}
+
+	oSymTable->length = 0;
+
+	return oSymTable;
+}
