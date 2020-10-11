bd9aee5ef64941c928626da60dcc48b00a7f5b89
Nicholas Padmanabhan <nicholaspad@princeton.edu>
2020-10-11 16:46:25 -0700

    finished expand

--- symtable/symtablehash.c
+++ symtable/symtablehash.c
@@ -23,7 +23,7 @@ struct Binding {
 
 /*
    Represents the hash table, consisting of an array of linked lists
-   of bindings and a variable to store the number of bindings.
+   of bindings, the number of bindings, and the number of buckets.
  */
 struct SymTable {
 	/* Pointer to buckets array */
@@ -35,7 +35,7 @@ struct SymTable {
 };
 
 /*
-   Return a hash code for pcKey that is between 0 and uBucketCount-1,
+   Returns a hash code for pcKey that is between 0 and uBucketCount-1,
    inclusive.
  */
 static size_t SymTable_hash(const char *pcKey, size_t uBucketCount) {
@@ -49,6 +49,79 @@ static size_t SymTable_hash(const char *pcKey, size_t uBucketCount) {
 		uHash = uHash * HASH_MULTIPLIER + (size_t) pcKey[u];
 
 	return uHash % uBucketCount;
+}
+
+/*
+   Returns the next bucket count based on the current bucket count,
+   needed during hash table expansion. Returns 0 if the maximum bucket
+   count is already in use, or if expansion is not yet needed.
+ */
+static size_t SymTable_nextSize(size_t uCurrentSize) {
+	switch (uCurrentSize) {
+	case INITIAL_SIZE:
+		return XXS;
+	case XXS:
+		return XS;
+	case XS:
+		return S;
+	case S:
+		return M;
+	case M:
+		return L;
+	case L:
+		return XL;
+	case XL:
+		return XXL;
+	default:
+		return 0;
+	}
+}
+
+/*
+   Creates and returns a new SymTable_T object with an expanded buckets
+   array and frees the old SymTable_T. Does not change the old
+   SymTable_T and returns NULL if expansion is not needed, if the
+   buckets array is already at maximum size, or if there is insufficient
+   memory.
+ */
+static SymTable_T SymTable_expand(SymTable_T oSymTable) {
+	SymTable_T newSymTable;
+	struct Binding *curr;
+	size_t uNewSize;
+	size_t b;
+	int res;
+	assert (oSymTable != NULL);
+
+	newSymTable = SymTable_new();
+
+	uNewSize = SymTable_nextSize(oSymTable->length);
+	if (uNewSize == 0) {
+		SymTable_free(newSymTable);
+		return NULL;
+	}
+
+	newSymTable->buckets = calloc(uNewSize, sizeof(struct Binding));
+	if (newSymTable->buckets == NULL) {
+		SymTable_free(newSymTable);
+		return NULL;
+	}
+
+	newSymTable->nBuckets = uNewSize;
+
+	for (b = 0; b < oSymTable->nBuckets; b++) {
+		for (curr = oSymTable->buckets[b]; curr != NULL;
+		     curr = curr->next) {
+			res = SymTable_put(newSymTable, curr->key, curr->value);
+			if (res == 0) {
+				SymTable_free(newSymTable);
+				return NULL;
+			}
+		}
+	}
+
+	SymTable_free(oSymTable);
+
+	return newSymTable;
 }
 
 SymTable_T SymTable_new(void) {
@@ -70,13 +143,13 @@ SymTable_T SymTable_new(void) {
 void SymTable_free(SymTable_T oSymTable) {
 	struct Binding *prev;
 	struct Binding *curr;
-	size_t i;
+	size_t b;
 	assert(oSymTable != NULL);
 
-	for (i = 0; i < oSymTable->nBuckets; i++) {
+	for (b = 0; b < oSymTable->nBuckets; b++) {
 		prev = NULL;
 
-		for (curr = oSymTable->buckets[i]; curr != NULL;
+		for (curr = oSymTable->buckets[b]; curr != NULL;
 		     curr = curr->next) {
 			free(prev);
 			free((char *) curr->key);
@@ -97,11 +170,16 @@ size_t SymTable_getLength(SymTable_T oSymTable) {
 
 int SymTable_put(SymTable_T oSymTable, const char *pcKey,
                  const void *pvValue) {
+	SymTable_T temp;
 	struct Binding *prev;
 	struct Binding *curr;
 	size_t i;
 	assert(oSymTable != NULL);
 	assert(pcKey != NULL);
+
+	temp = SymTable_expand(oSymTable);
+	if (temp != NULL)
+		oSymTable = temp;
 
 	i = SymTable_hash(pcKey, oSymTable->nBuckets);
 
@@ -222,12 +300,12 @@ void SymTable_map(SymTable_T oSymTable,
                                   void *pvExtra),
                   const void *pvExtra) {
 	struct Binding *curr;
-	size_t i;
+	size_t b;
 	assert(oSymTable != NULL);
 	assert(pfApply != NULL);
 
-	for (i = 0; i < oSymTable->nBuckets; i++)
-		for (curr = oSymTable->buckets[i]; curr != NULL;
+	for (b = 0; b < oSymTable->nBuckets; b++)
+		for (curr = oSymTable->buckets[b]; curr != NULL;
 		     curr = curr->next)
 			pfApply(curr->key, (void *) curr->value, (void *) pvExtra);
 }
